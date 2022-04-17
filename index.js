dbd2feed7b7239531e939caef73e250009fb5424
manjugaji <manjugaji@gmail.com>
2022-04-17 05:47:04 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-// Import stylesheets
-import './style.css';
+'use strict';
+function logger() {
+  console.log('hello');
+}
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(2, 4);
+console.log(appleJuice);
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age2);
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+console.log(calcAge3(1991));
+
+const yearsUntillRetirement = (birthYear) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+};
+
+console.log(yearsUntillRetirement(1990));
